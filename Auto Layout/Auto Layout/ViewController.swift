a3595c1957679fdbee48a15aec953c294eef8042
Michelle <Michelle.cueva@gmail.com>
2019-08-07 17:45:27 -0400

    Adds question one

---
+++ Auto Layout/Auto Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by Michelle Cueva on 8/7/19.
+//  Copyright © 2019 Michelle Cueva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
